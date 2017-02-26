67055aa5ecfad8e915d238efa05b5f3a01cdb6ad
Marcus Lin <linfaimom@gmail.com>
2017-02-26 23:32:10 +0800

    完成 footer 的编写

--- src/components/CAFooter.js
+++ src/components/CAFooter.js
@@ -2,16 +2,26 @@
  * Created by marcus on 2017/2/26.
  */
 import React from "react";
-import {grey800} from "material-ui/styles/colors";
+import {grey800, fullWhite, grey500} from "material-ui/styles/colors";
 
 const style = {
-    backgroundColor: grey800,
-    height: "64px"
+    container: {
+        backgroundColor: grey800,
+        height: "64px",
+        display: "flex"
+    },
+    siteInfo: {
+        margin: "auto",
+        textAlign: "center",
+        color: fullWhite
+    }
 };
 
 const CAFooter = (props) => (
-    <footer id={props.id} style={style}>
-
+    <footer id={props.id} style={style.container}>
+        <p style={style.siteInfo}>
+            Copyright © 2017 <a href="#" style={{color: grey500}}>XZCA</a>. All Rights Reserved.
+        </p>
     </footer>
 );
 
